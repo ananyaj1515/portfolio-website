a240ea057e0a7976dd415c10d8448e821e8ef186
Ananya Jain <ananyaj1515@gmail.com>
2025-06-27 10:45:20 +0530

    minor change

--- personal-site/src/pages/About.jsx
+++ personal-site/src/pages/About.jsx
@@ -29,29 +29,27 @@ const About = () => {
         <div className="py-10 flex flex-col">
           <h3 className="subhead-text">My Skills:</h3>
         </div>
-          <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 w-full">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 w-full">
            {[
-  { name: "Frontend", color: "bg-purple-600" },
-  { name: "Backend", color: "bg-blue-600" },
-  { name: "AI", color: "bg-pink-600" },
-  { name: "Database", color: "bg-purple-300" },
-  { name: "Language", color: "bg-blue-300" },
-  { name: "Software", color: "bg-pink-300" },
-  { name: "Version Control", color: "bg-green-100" },
+  { name: "Frontend", gradient: "from-purple-600", bg: "bg-purple-600" },
+  { name: "Backend", gradient: "from-blue-600", bg: "bg-blue-600" },
+  { name: "AI", gradient: "from-pink-600", bg: "bg-pink-600" },
+  { name: "Database", gradient: "from-purple-300", bg: "bg-purple-300" },
+  { name: "Language", gradient: "from-blue-300", bg: "bg-blue-300" },
+  { name: "Software", gradient: "from-pink-300", bg: "bg-pink-300" },
+  { name: "Version Control", gradient: "from-green-100", bg: "bg-green-100" },
 ].map((type) => (
   <div key={type.name}>
-    <h2 className={`text-xl font-semibold mb-4 capitalize text-white`}>
+    <h2 className={`text-xl font-semibold mb-4 capitalize bg-gradient-to-r ${type.gradient} to-white bg-clip-text text-transparent `}>
       {type.name}
     </h2>
-    <div className={`p-3 grid grid-cols-3 gap-4 ${type.color} bg-opacity-50 rounded-xl`}  style={{
-              boxShadow: '0 0 10px #ffffff',
-            }}>
+    <div className={`p-3 grid grid-cols-3 gap-4 ${type.bg} bg-opacity-50 rounded-xl border-[0.5px] border-white items-center`}>
       {skills
         .filter((skill) => skill.type === type.name)
         .map((skill) => (
           <div
             key={skill.name}
-            className={`w-20 h-20 btn-front bg-blue-300 bg-opacity-15 rounded-xl flex justify-center items-center shadow-lg`}
+            className={`w-20 h-20 border border-white btn-front bg-blue-300 bg-opacity-15 rounded-xl flex justify-center items-center shadow-lg`}
            
           >
             <img
